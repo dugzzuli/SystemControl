fbc86b0f6767030be79b4b2cb7b1da02b15957cb
dugking <bingwei2642@qq.com>
2019-03-24 21:48:10 +0800

    加载数据之后,将数据化可视化出来

--- SystemControl/Operate.Designer.cs
+++ SystemControl/Operate.Designer.cs
@@ -28,16 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Stepmethod = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.openfile = new System.Windows.Forms.Button();
-            this.openurl = new System.Windows.Forms.Button();
-            this.opendb = new System.Windows.Forms.Button();
             this.generate = new System.Windows.Forms.Button();
+            this.opendb = new System.Windows.Forms.Button();
+            this.openurl = new System.Windows.Forms.Button();
             this.openmat = new System.Windows.Forms.Button();
+            this.openfile = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvLearningSource = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileDataName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.attributeLength = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.samplesNUm = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.graphInput = new ZedGraph.ZedGraphControl();
             this.Stepmethod.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLearningSource)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // Stepmethod
@@ -48,37 +67,63 @@
             this.Stepmethod.Location = new System.Drawing.Point(0, 0);
             this.Stepmethod.Name = "Stepmethod";
             this.Stepmethod.SelectedIndex = 0;
-            this.Stepmethod.Size = new System.Drawing.Size(772, 552);
+            this.Stepmethod.Size = new System.Drawing.Size(772, 593);
             this.Stepmethod.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.generate);
-            this.tabPage1.Controls.Add(this.opendb);
-            this.tabPage1.Controls.Add(this.openurl);
-            this.tabPage1.Controls.Add(this.openmat);
-            this.tabPage1.Controls.Add(this.openfile);
+            this.tabPage1.Controls.Add(this.groupBox5);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(764, 526);
+            this.tabPage1.Size = new System.Drawing.Size(764, 567);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "预处理";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // generate
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(764, 526);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "聚类";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.generate.Location = new System.Drawing.Point(577, 21);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(82, 34);
+            this.generate.TabIndex = 0;
+            this.generate.Text = "生成数据";
+            this.generate.UseVisualStyleBackColor = true;
+            // 
+            // opendb
+            // 
+            this.opendb.Location = new System.Drawing.Point(432, 21);
+            this.opendb.Name = "opendb";
+            this.opendb.Size = new System.Drawing.Size(93, 34);
+            this.opendb.TabIndex = 0;
+            this.opendb.Text = "从数据库打开";
+            this.opendb.UseVisualStyleBackColor = true;
+            // 
+            // openurl
+            // 
+            this.openurl.Location = new System.Drawing.Point(286, 21);
+            this.openurl.Name = "openurl";
+            this.openurl.Size = new System.Drawing.Size(96, 34);
+            this.openurl.TabIndex = 0;
+            this.openurl.Text = "打开链接数据";
+            this.openurl.UseVisualStyleBackColor = true;
+            // 
+            // openmat
+            // 
+            this.openmat.Location = new System.Drawing.Point(158, 21);
+            this.openmat.Name = "openmat";
+            this.openmat.Size = new System.Drawing.Size(82, 34);
+            this.openmat.TabIndex = 0;
+            this.openmat.Text = "打开Mat文件";
+            this.openmat.UseVisualStyleBackColor = true;
             // 
             // openfile
             // 
-            this.openfile.Location = new System.Drawing.Point(26, 19);
+            this.openfile.Location = new System.Drawing.Point(20, 21);
             this.openfile.Name = "openfile";
             this.openfile.Size = new System.Drawing.Size(82, 34);
             this.openfile.TabIndex = 0;
@@ -86,52 +131,176 @@
             this.openfile.UseVisualStyleBackColor = true;
             this.openfile.Click += new System.EventHandler(this.openfile_Click);
             // 
-            // openurl
+            // tabPage2
             // 
-            this.openurl.Location = new System.Drawing.Point(238, 19);
-            this.openurl.Name = "openurl";
-            this.openurl.Size = new System.Drawing.Size(96, 34);
-            this.openurl.TabIndex = 0;
-            this.openurl.Text = "打开链接数据";
-            this.openurl.UseVisualStyleBackColor = true;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(764, 567);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "聚类";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // opendb
+            // groupBox1
             // 
-            this.opendb.Location = new System.Drawing.Point(360, 19);
-            this.opendb.Name = "opendb";
-            this.opendb.Size = new System.Drawing.Size(93, 34);
-            this.opendb.TabIndex = 0;
-            this.opendb.Text = "从数据库打开";
-            this.opendb.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.openfile);
+            this.groupBox1.Controls.Add(this.generate);
+            this.groupBox1.Controls.Add(this.openmat);
+            this.groupBox1.Controls.Add(this.opendb);
+            this.groupBox1.Controls.Add(this.openurl);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(748, 79);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "训练数据";
             // 
-            // generate
+            // groupBox2
             // 
-            this.generate.Location = new System.Drawing.Point(493, 19);
-            this.generate.Name = "generate";
-            this.generate.Size = new System.Drawing.Size(82, 34);
-            this.generate.TabIndex = 0;
-            this.generate.Text = "生成数据";
-            this.generate.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.dgvLearningSource);
+            this.groupBox2.Location = new System.Drawing.Point(9, 176);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 380);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "原始数据";
             // 
-            // openmat
+            // dgvLearningSource
             // 
-            this.openmat.Location = new System.Drawing.Point(131, 19);
-            this.openmat.Name = "openmat";
-            this.openmat.Size = new System.Drawing.Size(82, 34);
-            this.openmat.TabIndex = 0;
-            this.openmat.Text = "打开Mat文件";
-            this.openmat.UseVisualStyleBackColor = true;
+            this.dgvLearningSource.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLearningSource.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvLearningSource.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvLearningSource.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLearningSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLearningSource.Location = new System.Drawing.Point(3, 17);
+            this.dgvLearningSource.Name = "dgvLearningSource";
+            this.dgvLearningSource.RowTemplate.Height = 23;
+            this.dgvLearningSource.Size = new System.Drawing.Size(300, 360);
+            this.dgvLearningSource.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.attributeLength);
+            this.groupBox3.Controls.Add(this.samplesNUm);
+            this.groupBox3.Controls.Add(this.fileDataName);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 92);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(303, 78);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "数据信息";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件名称：";
+            // 
+            // fileDataName
+            // 
+            this.fileDataName.AutoSize = true;
+            this.fileDataName.Location = new System.Drawing.Point(77, 21);
+            this.fileDataName.Name = "fileDataName";
+            this.fileDataName.Size = new System.Drawing.Size(41, 12);
+            this.fileDataName.TabIndex = 1;
+            this.fileDataName.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "属性：";
+            // 
+            // attributeLength
+            // 
+            this.attributeLength.AutoSize = true;
+            this.attributeLength.Location = new System.Drawing.Point(195, 45);
+            this.attributeLength.Name = "attributeLength";
+            this.attributeLength.Size = new System.Drawing.Size(41, 12);
+            this.attributeLength.TabIndex = 1;
+            this.attributeLength.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "样本数目：";
+            // 
+            // samplesNUm
+            // 
+            this.samplesNUm.AutoSize = true;
+            this.samplesNUm.Location = new System.Drawing.Point(77, 45);
+            this.samplesNUm.Name = "samplesNUm";
+            this.samplesNUm.Size = new System.Drawing.Size(41, 12);
+            this.samplesNUm.TabIndex = 1;
+            this.samplesNUm.Text = "label2";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(335, 92);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(421, 199);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "当前属性";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.graphInput);
+            this.groupBox5.Location = new System.Drawing.Point(335, 298);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(421, 255);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "可视化";
+            // 
+            // graphInput
+            // 
+            this.graphInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphInput.Location = new System.Drawing.Point(3, 17);
+            this.graphInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.graphInput.Name = "graphInput";
+            this.graphInput.ScrollGrace = 0D;
+            this.graphInput.ScrollMaxX = 0D;
+            this.graphInput.ScrollMaxY = 0D;
+            this.graphInput.ScrollMaxY2 = 0D;
+            this.graphInput.ScrollMinX = 0D;
+            this.graphInput.ScrollMinY = 0D;
+            this.graphInput.ScrollMinY2 = 0D;
+            this.graphInput.Size = new System.Drawing.Size(415, 235);
+            this.graphInput.TabIndex = 5;
+            this.graphInput.UseExtendedPrintDialog = true;
+            this.graphInput.Load += new System.EventHandler(this.graphInput_Load);
+            this.graphInput.Click += new System.EventHandler(this.graphInput_Click);
             // 
             // Operate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(772, 552);
+            this.ClientSize = new System.Drawing.Size(772, 593);
             this.Controls.Add(this.Stepmethod);
             this.Name = "Operate";
             this.Text = "Operate";
             this.Stepmethod.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLearningSource)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -146,5 +315,18 @@
         private System.Windows.Forms.Button openurl;
         private System.Windows.Forms.Button openfile;
         private System.Windows.Forms.Button openmat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvLearningSource;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label attributeLength;
+        private System.Windows.Forms.Label samplesNUm;
+        private System.Windows.Forms.Label fileDataName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private ZedGraph.ZedGraphControl graphInput;
     }
 }
--- SystemControl/Operate.cs
+++ SystemControl/Operate.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Accord.Controls;
+using Accord.IO;
+using Accord.Math;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,11 +12,16 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
 
 namespace SystemControl
 {
     public partial class Operate : Form
     {
+
+        string[] columnNames; // stores the column names for the loaded data
+        double[,] sourceMatrix;
+
         public Operate()
         {
             InitializeComponent();
@@ -22,41 +30,146 @@ namespace SystemControl
         private void openfile_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = "D:\\";//注意这里写路径时要用c:\\而不是c:\
-            openFileDialog.Filter = "文本文件|*.txt|csv文件|*.csv|所有文件|*.*";
+            openFileDialog.Filter = "文本文件|*.txt|csv文件|*.csv|Excel文件|*.xls|所有文件|*.*";
             openFileDialog.RestoreDirectory = true;
             openFileDialog.FilterIndex = 1;
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string fName = openFileDialog.FileName;
-
-
-                WebClient myWebClient = new WebClient();
-
-                //UploadFileUtil.UploadFile(fName, "http://127.0.0.1:8080/SpringNMF/demo/upload");
-                //myWebClient.UploadFile("http://localhost:8080/SpringNMF/demo/upload", "POST", fName);
-
-
-                var url = "http://localhost:8080/SpringNMF/demo/upload";
-                var log1 = fName;
-
-                var formDatas = new List<FormItemModel>();
-                //添加文件
-                formDatas.Add(new FormItemModel()
+                string extension = Path.GetExtension(fName);
+                if (extension == ".csv")
                 {
-                    Key = "file",
-                    Value = "",
-                    FileName = "log1.txt",
-                    FileContent = File.OpenRead(log1)
-                });
-              
-                //提交表单
-                var result =UploadFile.PostForm(url, formDatas);
-  
+                    CsvReader csvR = new CsvReader(fName, false);
 
-                MessageBox.Show("已上传");
+                    DataTable tableSource = csvR.ToTable();
+
+                   sourceMatrix = tableSource.ToMatrix(out columnNames);
+
+
+
+                    // Detect the kind of problem loaded.
+                    if (sourceMatrix.GetLength(1) == 2)
+                    {
+                        MessageBox.Show("Missing class column.");
+                    }
+                    else
+                    {
+                        this.dgvLearningSource.DataSource = tableSource;
+                        CreateScatterplot(graphInput, sourceMatrix);
+                      
+                    }
+
+                }
+
+
+                if (extension == ".xls" || extension == ".xlsx")
+                {
+                    ExcelReader db = new ExcelReader(fName, true, false);
+                    TableSelectDialog t = new TableSelectDialog(db.GetWorksheetList());
+                    if (t.ShowDialog(this) == DialogResult.OK)
+                    {
+
+                        DataTable tableSource = db.GetWorksheet(t.Selection);
+
+                        sourceMatrix = tableSource.ToMatrix(out columnNames);
+
+
+
+                        // Detect the kind of problem loaded.
+                        if (sourceMatrix.GetLength(1) == 2)
+                        {
+                            MessageBox.Show("Missing class column.");
+                        }
+                        else
+                        {
+                            this.dgvLearningSource.DataSource = tableSource;
+                          
+
+
+                            CreateScatterplot(graphInput, sourceMatrix);
+                        }
+                    }
+                }
             }
+            //uploadFile(fName);
+        }
+
+      
+        public void CreateScatterplot(ZedGraphControl zgc, double[,] graph)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            myPane.CurveList.Clear();
+
+            // Set the titles
+            myPane.Title.IsVisible = false;
+            myPane.XAxis.Title.Text = columnNames[0];
+            myPane.YAxis.Title.Text = columnNames[1];
+
+
+            // Classification problem
+            PointPairList list1 = new PointPairList(); // Z = -1
+            PointPairList list2 = new PointPairList(); // Z = +1
+
+            int lastIndex = graph.GetLength(1)-1;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[i, lastIndex] == -1)
+                    list1.Add(graph[i, 0], graph[i, 1]);
+                if (graph[i, lastIndex] == 1)
+                    list2.Add(graph[i, 0], graph[i, 1]);
+            }
+
+            // Add the curve
+            LineItem myCurve = myPane.AddCurve("G1", list1, Color.Blue, SymbolType.Diamond);
+            myCurve.Line.IsVisible = false;
+            myCurve.Symbol.Border.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Blue);
+
+            myCurve = myPane.AddCurve("G2", list2, Color.Green, SymbolType.Diamond);
+            myCurve.Line.IsVisible = false;
+            myCurve.Symbol.Border.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Green);
+
+
+            myPane.Fill = new Fill(Color.WhiteSmoke);
+
+            zgc.AxisChange();
+            zgc.Invalidate();
+        }
+
+        private static void uploadFile(string fName)
+        {
+            string url = "http://localhost:8080/SpringNMF/demo/upload";
+
+            List<FormItemModel> formDatas = new List<FormItemModel>();
+            //添加文件
+            formDatas.Add(new FormItemModel()
+            {
+                Key = "file",
+                Value = "",
+                FileName = Path.GetFileName(fName),
+                FileContent = File.OpenRead(fName)
+            });
+
+            //提交表单
+            string result = UploadFile.PostForm(url, formDatas);
+
+
+            MessageBox.Show("已上传" + "   返回信息：" + result.ToString());
+        }
+
+        private void graphInput_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void graphInput_Click(object sender, EventArgs e)
+        {
+            ScatterplotBox.Show("大图", sourceMatrix.GetColumns(0, 1), sourceMatrix.GetColumn(3).ToInt32()).Hold();
         }
     }
+
+
 }
+
 
